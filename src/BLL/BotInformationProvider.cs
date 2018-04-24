052bf1c824eb586b83d87e184feeaea2650feac7
Stefan Over <herdosratos@googlemail.com>
2018-04-24 21:22:05 +0200

    Added "days" to up-time
    If days were not included, the "up-time" would show only the up-time
    during the last 24 hours

--- src/BLL/BotInformationProvider.cs
+++ src/BLL/BotInformationProvider.cs
@@ -23,7 +23,7 @@
                 {
                     new EmbedField("Environment", _runtimeInformation.Environment, true), 
                     new EmbedField("Version", _runtimeInformation.Version, true), 
-                    new EmbedField("UP-time", (DateTime.Now.ToUniversalTime() - _runtimeInformation.StartTime).ToString(@"hh\:mm\:ss"), true), 
+                    new EmbedField("UP-time", (DateTime.Now.ToUniversalTime() - _runtimeInformation.StartTime).ToString(@"dd\.hh\:mm\:ss"), true), 
                     new EmbedField("Start-time", _runtimeInformation.StartTime.ToString("dd.MM.yyyy HH:mm:ss") + " UTC", false), 
                     new EmbedField("Server time", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss \"UTC\"zzz"), false)
                 }
