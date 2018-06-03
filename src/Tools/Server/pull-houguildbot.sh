5c68a1dcb9c48aeb6129ecb28a9d8ca55859ee1a
Stefan Over <herdosratos@googlemail.com>
2018-06-03 14:11:43 +0200

    added additional helper scripts

---
+++ src/Tools/Server/pull-houguildbot.sh
@@ -0,0 +1,6 @@
+REGISTRY_HOST="$(sed -nr "/^\[Network\]/ { :l /^RegistryHost[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./settings.registry.production.ini)"
+REGISTRY_EXTERNAL_PORT="$(sed -nr "/^\[Network\]/ { :l /^RegistryExternalPort[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./settings.registry.production.ini)"
+
+docker login $REGISTRY_HOST:$REGISTRY_EXTERNAL_PORT
+docker pull $REGISTRY_HOST:$REGISTRY_EXTERNAL_PORT/houguildbot:latest
+docker logout $REGISTRY_HOST:$REGISTRY_EXTERNAL_PORT
---
+++ src/Tools/Server/run-houguildbot.sh
@@ -0,0 +1,8 @@
+REGISTRY_HOST="$(sed -nr "/^\[Network\]/ { :l /^RegistryHost[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./settings.registry.production.ini)"
+REGISTRY_EXTERNAL_PORT="$(sed -nr "/^\[Network\]/ { :l /^RegistryExternalPort[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./settings.registry.production.ini)"
+
+docker run -d \
+  --name houguildbot \
+  --restart unless-stopped \
+  -v hou-guildbot-logs-volume:/app/logs \
+  $REGISTRY_HOST:$REGISTRY_EXTERNAL_PORT/houguildbot:latest
