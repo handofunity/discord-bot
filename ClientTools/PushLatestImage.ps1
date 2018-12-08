be76e912b5e9a36f6b7d70498422568660c2fdfe
Stefan Over <herdosratos@googlemail.com>
2018-12-08 10:30:31 +0100

    Added push latest image powershell script again

---
+++ ClientTools/PushLatestImage.ps1
@@ -0,0 +1,57 @@
+function Get-IniContent {
+    [CmdletBinding()]  
+    Param(  
+        [ValidateNotNullOrEmpty()]  
+        [ValidateScript({(Test-Path $_) -and ((Get-Item $_).Extension -eq ".ini")})]  
+        [Parameter(ValueFromPipeline=$True,Mandatory=$True)]  
+        [string]$FilePath  
+    )  
+    $ini = @{}  
+        switch -regex -file $FilePath  
+        {  
+            "^\[(.+)\]$" # Section  
+            {  
+                $section = $matches[1]  
+                $ini[$section] = @{}  
+                $CommentCount = 0  
+            }  
+            "^(;.*)$" # Comment  
+            {  
+                if (!($section))  
+                {  
+                    $section = "No-Section"  
+                    $ini[$section] = @{}  
+                }  
+                $value = $matches[1]  
+                $CommentCount = $CommentCount + 1  
+                $name = "Comment" + $CommentCount  
+                $ini[$section][$name] = $value  
+            }   
+            "(.+?)\s*=\s*(.*)" # Key  
+            {  
+                if (!($section))  
+                {  
+                    $section = "No-Section"  
+                    $ini[$section] = @{}  
+                }  
+                $name,$value = $matches[1..2]  
+                $ini[$section][$name] = $value  
+            }  
+        }  
+        Return $ini  
+}
+# Load settings
+$settingsPath = "settings.houguildbot.ini"
+$absoluteSettingsPath = Join-Path -Path $PSScriptRoot -ChildPath $settingsPath
+$settings = Get-IniContent $absoluteSettingsPath
+# Set variables
+$sourceImage = "$($settings['Image']['SourceName']):$($settings['Image']['SourceTag'])"
+$targetImage = "$($settings['Network']['RegistryHost']):$($settings['Network']['RegistryPort'])/$($settings['Image']['TargetName']):$($settings['Image']['TargetTag'])"
+# Docker tag
+Start-Process "docker" -ArgumentList "tag $($sourceImage) $($targetImage)" -Wait -NoNewWindow
+# Docker login
+Start-Process "docker" -ArgumentList "login $($settings['Network']['RegistryHost']):$($settings['Network']['RegistryPort'])" -Wait
+# Docker push
+Start-Process "docker" -ArgumentList "push $($targetImage)" -Wait
+# Docker logout
+Start-Process "docker" -ArgumentList "logout $($settings['Network']['RegistryHost']):$($settings['Network']['RegistryPort'])" -Wait -NoNewWindow 
