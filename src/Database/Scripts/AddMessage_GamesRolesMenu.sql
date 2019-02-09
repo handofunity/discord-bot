409310c807a81ee77a196779f2c25705792cd0e8
Stefan Over <herdosratos@googlemail.com>
2019-02-09 12:57:37 +0100

    Changed message template for games menu

--- src/Database/Scripts/AddMessage_GamesRolesMenu.sql
+++ src/Database/Scripts/AddMessage_GamesRolesMenu.sql
@@ -8,13 +8,5 @@ VALUES
 (
 'GamesRolesMenu',
 'The message used as the role menu for games.',
-'**GAME MENU**
-Game: `{0}`
-
-To add yourself to the game and all functionalities associated with it, add your :joystick: reaction.
-To remove yourself from the game and all functionalities associated with it, add your :joystick: reaction.
-
-Functionalities include, but are not limited to:
-- Being able to see any channels with the game.
-- Being ping-able with the game role.'
+'Game: `{0}` - Use the :joystick: reaction to add or remove the primary game role.'
 )
