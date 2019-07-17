2ccd7af9dd0bee43c733ecafa12ca0bc4474bc12
Stefan Over <herdosratos@googlemail.com>
2019-07-17 18:19:37 +0200

    Updated NuGet references, increased version number

--- src/Core/Runner.cs
+++ src/Core/Runner.cs
@@ -16,7 +16,7 @@
 
     public class Runner
     {
-        private static readonly Version BotVersion = new Version(3, 1, 0);
+        private static readonly Version BotVersion = new Version(3, 1, 1);
 
         private ILogger<Runner> _logger;
 
