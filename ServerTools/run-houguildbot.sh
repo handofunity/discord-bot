c390ff8240bb30d11d0dd7b2df725c9b85b0a54c
Stefan Over <herdosratos@googlemail.com>
2019-10-26 12:50:32 +0200

    Limited runtime for docker container

--- ServerTools/run-houguildbot.sh
+++ ServerTools/run-houguildbot.sh
@@ -4,6 +4,10 @@ REGISTRY_EXTERNAL_PORT="$(sed -nr "/^\[Network\]/ { :l /^RegistryExternalPort[ ]
 docker run -d \
   --name houguildbot \
   --restart unless-stopped \
+  --cap-drop=ALL \
+  --cap-add=SETFCAP \
+  --memory=192m \
+  --memory-swap=400m \
   -v /var/log/hand-of-unity/guild-bot:/app/logs \
   -v /usr/share/fonts/:/usr/share/fonts/external/ \
   $REGISTRY_HOST:$REGISTRY_EXTERNAL_PORT/houguildbotwebhost:latest
