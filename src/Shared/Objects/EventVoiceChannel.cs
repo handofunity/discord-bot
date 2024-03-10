836e5656f0aa3ca1edcc40438d5faab7d7ad91cd
Stefan Over <herdosratos@googlemail.com>
2024-03-10 18:48:42 +0100

    Enhanced Group Display Name
    Enhanced the display name of the group in the EventVoiceChannel class to include emoji representations for each group number, in order to make group identities more recognizable and intuitive.

--- src/Shared/Objects/EventVoiceChannel.cs
+++ src/Shared/Objects/EventVoiceChannel.cs
@@ -30,7 +30,40 @@ public class EventVoiceChannel
                              byte maxUsersInChannel)
     {
         GroupNumber = groupNumber;
-        DisplayName = $"UNITS {appointmentId}: Group {groupNumber}";
+        DisplayName = $"UNITS {appointmentId}: Group {groupNumber} ({GetGroupEmoji(groupNumber)})";
         MaxUsersInChannel = maxUsersInChannel;
+    }
+
+    private static string GetGroupEmoji(int groupNumber)
+    {
+        return groupNumber switch
+        {
+            1 => "\uD83D\uDC31 Cat",
+            2 => "\uD83E\uDD8D Gorilla",
+            3 => "\uD83D\uDC14 Chicken",
+            4 => "\uD83E\uDD96 T-Rex",
+            5 => "\uD83D\uDC18 Elephant",
+            6 => "\uD83D\uDC27 Penguin",
+            7 => "\uD83E\uDD8A Fox",
+            8 => "\uD83E\uDD98 Kangaroo",
+            9 => "\uD83D\uDC2C Dolphin",
+            10 => "\uD83E\uDDA2 Swan",
+            11 => "\uD83E\uDD81 Lion",
+            12 => "\uD83E\uDD80 Crab",
+            13 => "\uD83E\uDD8B Butterfly",
+            14 => "\uD83E\uDD89 Owl",
+            15 => "\uD83E\uDD88 Shark",
+            16 => "\uD83D\uDC3A Wolf",
+            17 => "\uD83D\uDC09 Dragon",
+            18 => "\uD83D\uDC11 Sheep",
+            19 => "\uD83E\uDD93 Zebra",
+            20 => "\uD83D\uDC0C Snail",
+            21 => "\uD83E\uDD94 Hedgehog",
+            22 => "\uD83D\uDC3C Panda",
+            23 => "\uD83D\uDC22 Turtle",
+            24 => "\uD83E\uDDA9 Flamingo",
+            25 => "\uD83E\uDD86 Duck",
+            _ => throw new ArgumentOutOfRangeException(nameof(groupNumber))
+        };
     }
 }
