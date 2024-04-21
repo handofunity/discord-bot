21e902787ee13552cf66f328fb991f7f93cc0b06
Stefan Over <herdosratos@googlemail.com>
2024-04-21 15:35:17 +0200

    Update bot version to 11.1.0
    The bot version number has been incremented to 11.1.0 in Runner.cs. This reflects the new changes that have been implemented in the application.

--- src/Core/Runner.cs
+++ src/Core/Runner.cs
@@ -2,7 +2,7 @@
 
 public class Runner
 {
-    private static readonly Version _botVersion = new(11, 0, 0);
+    private static readonly Version _botVersion = new(11, 1, 0);
 
     private BackgroundJobServer? _backgroundJobServer;
     private ILogger<Runner>? _logger;
