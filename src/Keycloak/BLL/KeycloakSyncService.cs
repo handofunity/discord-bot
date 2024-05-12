1829e97061b01318bdd5c7fa16e5c4f29a9b2ed6
Stefan Over <herdosratos@googlemail.com>
2024-05-12 21:00:28 +0200

    Only run one Keycloak sync at a time

--- src/Keycloak/BLL/KeycloakSyncService.cs
+++ src/Keycloak/BLL/KeycloakSyncService.cs
@@ -2,6 +2,8 @@
 
 internal class KeycloakSyncService : IKeycloakSyncService
 {
+    private static readonly SemaphoreSlim SemaphoreSlim = new(1, 1);
+
     private readonly IDiscordAccess _discordAccess;
     private readonly IKeycloakDiscordComparer _keycloakDiscordComparer;
     private readonly IDynamicConfiguration _dynamicConfiguration;
@@ -108,70 +110,78 @@ internal class KeycloakSyncService : IKeycloakSyncService
     // Do NOT implement this as explicit implementation, as it cannot be triggered by hangfire then!
     public async Task SyncAllUsersAsync()
     {
-        if (_dynamicConfiguration.KeycloakEndpoints.Length == 0)
+        await SemaphoreSlim.WaitAsync();
+        try
         {
-            _logger.LogWarning("No Keycloak access configured");
-            return;
+            if (_dynamicConfiguration.KeycloakEndpoints.Length == 0)
+            {
+                _logger.LogWarning("No Keycloak access configured");
+                return;
+            }
+
+            if (!_discordAccess.IsConnected || !_discordAccess.IsGuildAvailable)
+                return;
+
+            foreach (var keycloakEndpoint in _dynamicConfiguration.KeycloakEndpoints)
+            {
+                if (!IsValidEndpoint(keycloakEndpoint))
+                    continue;
+
+                var keycloakState = await _keycloakUserGroupAggregator.AggregateCurrentStateAsync(keycloakEndpoint);
+                if (keycloakState is null)
+                    continue;
+
+                // Get Discord users and roles
+                var allDiscordUsers = await _discordAccess.GetAllUsersAsync();
+                SanitizeDiscordUsersAndRoles(allDiscordUsers, keycloakState.ConfiguredKeycloakGroups);
+
+                // Get diff between Discord and Keycloak
+                var diff = _keycloakDiscordComparer.GetDiff(keycloakState, allDiscordUsers);
+
+                _logger.LogInformation("Sending {@Diff} to Keycloak at {Address} ...",
+                                       diff,
+                                       keycloakEndpoint.BaseUrl);
+                var result = await SyncDiffToEndpointAsync(keycloakEndpoint, diff);
+
+                _logger.LogInformation("Sent {@Result} to Keycloak at {Address}",
+                                       result,
+                                       keycloakEndpoint.BaseUrl);
+                var utcNow = DateTime.UtcNow;
+                var notificationRequired = utcNow is { Hour: 15, Minute: < 15 };
+                var hasChanges = result.AddedUsers > 0
+                              || result.DisabledUsers > 0
+                              || result.LoggedOutUsers > 0
+                              || result.EnabledUsers > 0
+                              || result.UpdatedDetails > 0
+                              || result.AssignedGroupMemberships > 0
+                              || result.RemovedGroupMemberships > 0;
+                var shouldPostResults = notificationRequired || hasChanges;
+                if (!shouldPostResults)
+                    continue;
+
+                var sb = new StringBuilder($"**Keycloak Sync:** Synchronized {allDiscordUsers.Length} users with Keycloak");
+                sb.AppendLine();
+                if (result.AddedUsers > 0)
+                    sb.AppendLine($"Added {result.AddedUsers} users.");
+                if (result.DisabledUsers > 0)
+                    sb.AppendLine($"Disabled {result.DisabledUsers} users.");
+                if (result.LoggedOutUsers > 0)
+                    sb.AppendLine($"Logged out {result.LoggedOutUsers} users.");
+                if (result.EnabledUsers > 0)
+                    sb.AppendLine($"Enabled {result.EnabledUsers} users.");
+                if (result.UpdatedDetails > 0)
+                    sb.AppendLine($"Updated {result.UpdatedDetails} user details (avatar, nickname, ...).");
+                if (result.AssignedGroupMemberships > 0)
+                    sb.AppendLine($"Assigned {result.AssignedGroupMemberships} users to groups.");
+                if (result.RemovedGroupMemberships > 0)
+                    sb.AppendLine($"Removed {result.RemovedGroupMemberships} users from groups.");
+
+                await _discordAccess.LogToDiscordAsync(sb.ToString());
+            }
         }
-
-        if (!_discordAccess.IsConnected || !_discordAccess.IsGuildAvailable)
-            return;
-
-        foreach (var keycloakEndpoint in _dynamicConfiguration.KeycloakEndpoints)
+        finally
         {
-            if (!IsValidEndpoint(keycloakEndpoint))
-                continue;
-
-            var keycloakState = await _keycloakUserGroupAggregator.AggregateCurrentStateAsync(keycloakEndpoint);
-            if (keycloakState is null)
-                continue;
-
-            // Get Discord users and roles
-            var allDiscordUsers = await _discordAccess.GetAllUsersAsync();
-            SanitizeDiscordUsersAndRoles(allDiscordUsers, keycloakState.ConfiguredKeycloakGroups);
-
-            // Get diff between Discord and Keycloak
-            var diff = _keycloakDiscordComparer.GetDiff(keycloakState, allDiscordUsers);
-
-            _logger.LogInformation("Sending {@Diff} to Keycloak at {Address} ...",
-                                   diff,
-                                   keycloakEndpoint.BaseUrl);
-            var result = await SyncDiffToEndpointAsync(keycloakEndpoint, diff);
-
-            _logger.LogInformation("Sent {@Result} to Keycloak at {Address}",
-                                   result,
-                                   keycloakEndpoint.BaseUrl);
-            var utcNow = DateTime.UtcNow;
-            var notificationRequired = utcNow is { Hour: 15, Minute: < 15 };
-            var hasChanges = result.AddedUsers > 0
-                          || result.DisabledUsers > 0
-                          || result.LoggedOutUsers > 0
-                          || result.EnabledUsers > 0
-                          || result.UpdatedDetails > 0
-                          || result.AssignedGroupMemberships > 0
-                          || result.RemovedGroupMemberships > 0;
-            var shouldPostResults = notificationRequired || hasChanges;
-            if (!shouldPostResults)
-                continue;
-
-            var sb = new StringBuilder($"**Keycloak Sync:** Synchronized {allDiscordUsers.Length} users with Keycloak");
-            sb.AppendLine();
-            if (result.AddedUsers > 0)
-                sb.AppendLine($"Added {result.AddedUsers} users.");
-            if (result.DisabledUsers > 0)
-                sb.AppendLine($"Disabled {result.DisabledUsers} users.");
-            if (result.LoggedOutUsers > 0)
-                sb.AppendLine($"Logged out {result.LoggedOutUsers} users.");
-            if (result.EnabledUsers > 0)
-                sb.AppendLine($"Enabled {result.EnabledUsers} users.");
-            if (result.UpdatedDetails > 0)
-                sb.AppendLine($"Updated {result.UpdatedDetails} user details (avatar, nickname, ...).");
-            if (result.AssignedGroupMemberships > 0)
-                sb.AppendLine($"Assigned {result.AssignedGroupMemberships} users to groups.");
-            if (result.RemovedGroupMemberships > 0)
-                sb.AppendLine($"Removed {result.RemovedGroupMemberships} users from groups.");
-
-            await _discordAccess.LogToDiscordAsync(sb.ToString());
+            SemaphoreSlim.Release();
         }
     }
 
